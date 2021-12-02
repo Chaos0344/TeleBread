dd3bd5aacf7e1ea994b36198cddcd6d13d0e9748
Blake Roetzel <baroetzel2@yahoo.com>
2021-12-02 00:07:38 -0600

    Still rebuilding from old project.

---
+++ TeleBreadService/Commands.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using System.Data.SqlClient;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InputFiles;
+using System.Timers;
+using Telegram.Bot.Requests;
+using Telegram.Bot.Types.ReplyMarkups;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace TeleBreadService
+{
+    public class Commands
+    {
+        public Commands()
+        {
+        }
+
+        /// <summary>
+        /// Returns basic instructions on how to get more info or begin using the bot.
+        /// </summary>
+        /// <param name="botClient"></param>
+        /// <param name="e"></param>
+        public async void start(ITelegramBotClient botClient, Update e)
+        {
+            botClient.SendTextMessageAsync(e.Message.Chat.Id,
+                "Hello! TeleBread is a bot primarily made for goofing around with friends in a group chat!\n" +
+                "Please visit telebread.net if you want additional information about our bot!");
+            botClient.SendTextMessageAsync(e.Message.Chat.Id,
+                "If you already know how to use this bot and wish to proceed, please use /group to link this as " +
+                "your group chat, or /private to link it as your private chat.");
+        }
+    }
+}
--- TeleBreadService/Program.cs
+++ TeleBreadService/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration.Install;
+using System.Reflection;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
@@ -12,14 +14,38 @@ namespace TeleBreadService
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        static void Main()
+        static void Main(string[] args)
         {
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[]
-            {
-                new Service1()
-            };
-            ServiceBase.Run(ServicesToRun);
+#if DEBUG
+            Service1 myService = new Service1();
+            myService.OnDebug();
+            System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite);
         }
+#else
+            if (Environment.UserInteractive)
+            {
+                string parameter = String.Concat(args);
+                switch (parameter)
+                {
+                    case "--install":
+                        ManagedInstallerClass.InstallHelper(new[] {Assembly.GetExecutingAssembly().Location});
+                        break;
+                    case "--uninstall":
+                        ManagedInstallerClass.InstallHelper(new[] {Assembly.GetExecutingAssembly().Location});
+                        break;
+                }
+            }
+            else
+            {
+
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[]
+                {
+                    new Service1()
+                };
+                ServiceBase.Run(ServicesToRun);
+            }
+        }
+#endif
     }
 }
